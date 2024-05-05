ba882745500cda9f440e1e860c2ddc3fa11c11c1
Brun0HC <bruno.cobuci@biotronica.tech>
2024-05-04 21:08:15 -0300

    feat: implement 3 players game logic

--- app/src/main/java/com/example/pedrapapeltesoura/ui/GameActivity.kt
+++ app/src/main/java/com/example/pedrapapeltesoura/ui/GameActivity.kt
@@ -1,66 +1,87 @@
 package com.example.pedrapapeltesoura.ui
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 import com.example.pedrapapeltesoura.R
 import com.example.pedrapapeltesoura.databinding.ActivityGameBinding
 import com.example.pedrapapeltesoura.jogadasMao.JogadasMao
+import com.example.pedrapapeltesoura.model.Constant.NUMERO_JOGADORES
 
 class GameActivity : AppCompatActivity() {
     private val agb: ActivityGameBinding by lazy {
         ActivityGameBinding.inflate(layoutInflater)
     }
-    private var numeroJogadores: Int = 0
     private var mao: JogadasMao? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(agb.root)
 
-        val numeroJogadores = intent.getIntExtra("numeroJogadores", 2)
+        val numeroJogadores = intent.getIntExtra(NUMERO_JOGADORES, 0)
 
-        when (numeroJogadores) {
-            2 -> iniciarJogo1v1()
-            //3 -> iniciarJogo1v1v1()
+        if (numeroJogadores == 2){
+            iniciarJogo1v1()}
+        else if(numeroJogadores == 3){
+            iniciarJogo1v2()}
+        else{
+            Toast.makeText(this, "ERRO DA INTENT", Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun iniciarJogo1v1() {
+        setupUI()
+        agb.jogarBt.setOnClickListener {
+            val bot = jogadaBot()
+            dueloUmVsUm(mao, bot)
+            agb.imgBot.setImageResource(getEscolhaBot(bot))
+            agb.botTv.visibility= View.VISIBLE
+            agb.imgBot.visibility = View.VISIBLE
         }
 
     }
-
-    private fun iniciarJogo1v1() {
+    private fun iniciarJogo1v2() {
         setupUI()
+        agb.jogarBt.setOnClickListener {
+            val bot1 = jogadaBot()
+            val bot2 = jogadaBot()
+            dueloUmVsDois(mao, bot1, bot2)
+
+            agb.imgBot.setImageResource(getEscolhaBot(bot1))
+            agb.botTv.visibility= View.VISIBLE
+            agb.imgBot.visibility = View.VISIBLE
+            agb.imgBot2.setImageResource(getEscolhaBot(bot2))
+            agb.botTv2.visibility= View.VISIBLE
+            agb.imgBot2.visibility = View.VISIBLE
+        }
+
     }
 
     private fun setupUI() {
         agb.toolbarIn.toolbar.apply {
-             title = getString(R.string.app_name)
+            title = getString(R.string.app_name)
             subtitle = this@GameActivity.javaClass.simpleName
             setSupportActionBar(this)
         }
 
-        agb.imgStone.setOnClickListener {
+        agb.imgPedra.setOnClickListener {
             mao = JogadasMao.PEDRA
         }
 
-        agb.imgPaper.setOnClickListener {
+        agb.imgPapel.setOnClickListener {
             mao = JogadasMao.PAPEL
         }
 
-        agb.imgScizor.setOnClickListener {
+        agb.imgTesoura.setOnClickListener {
             mao = JogadasMao.TESOURA
-        }
-
-        agb.jogarBt.setOnClickListener {
-            val aplicativo1 = jogadaBot()
-            mao?.let { it1 -> dueloJokenpo(it1, aplicativo1) }
         }
     }
 
     private fun jogadaBot(): JogadasMao {
-        val maos = JogadasMao.entries
-        return maos.random()
+        val mao = JogadasMao.entries
+        return mao.random()
     }
-    private fun dueloJokenpo(maoUsuario: JogadasMao, maoBot: JogadasMao): Any {
+    private fun dueloUmVsUm(maoUsuario: JogadasMao?, maoBot: JogadasMao): Any {
         return when {
             maoUsuario == maoBot -> exibirResultado("empate", maoBot)
             maoUsuario == JogadasMao.PEDRA && maoBot == JogadasMao.TESOURA ||
@@ -69,15 +90,63 @@ class GameActivity : AppCompatActivity() {
             else -> exibirResultado("derrota", maoBot)
         }
     }
+    private fun dueloUmVsDois(maoUsuario: JogadasMao?, bot1: JogadasMao, bot2: JogadasMao):Any {
+        return when {
+            // SÓ EMPATES
+            maoUsuario == bot1 && maoUsuario == bot2 -> exibirResultado("empate",null)
+            maoUsuario == JogadasMao.PEDRA && bot1 == JogadasMao.PEDRA && bot2 == JogadasMao.TESOURA-> exibirResultado("empate", null)
+            maoUsuario == JogadasMao.PEDRA && bot1 == JogadasMao.TESOURA && bot2 == JogadasMao.PEDRA-> exibirResultado("empate", null)
+            maoUsuario == JogadasMao.TESOURA && bot1 == JogadasMao.TESOURA && bot2 == JogadasMao.PAPEL-> exibirResultado("empate", null)
+            maoUsuario == JogadasMao.TESOURA && bot1 == JogadasMao.PAPEL && bot2 == JogadasMao.TESOURA-> exibirResultado("empate", null)
+            maoUsuario == JogadasMao.PAPEL && bot1 == JogadasMao.PAPEL && bot2 == JogadasMao.PEDRA-> exibirResultado("empate", null)
+            maoUsuario == JogadasMao.PAPEL && bot1 == JogadasMao.PEDRA && bot2 == JogadasMao.PAPEL-> exibirResultado("empate", null)
+            maoUsuario != bot1 && maoUsuario != bot2 && bot1 != bot2 -> exibirResultado("empate",null)
 
+            // SÓ VITÓRIAS
+            maoUsuario == JogadasMao.PAPEL && bot1 == JogadasMao.PEDRA && bot2 == JogadasMao.PEDRA-> exibirResultado("vitória", null)
+            maoUsuario == JogadasMao.PEDRA && bot1 == JogadasMao.TESOURA && bot2 == JogadasMao.TESOURA-> exibirResultado("vitória", null)
+            maoUsuario == JogadasMao.TESOURA && bot1 == JogadasMao.PAPEL && bot2 == JogadasMao.PAPEL-> exibirResultado("vitória", null)
 
-    private fun exibirResultado(resultado: String, maoBot: JogadasMao) {
-        val mensagem = when (resultado) {
-            "empate" -> "O jogo terminou empatado, o adversário também escolheu ${maoBot.name}"
-            "vitoria" -> "Você venceu, o adversário escolheu ${maoBot.name}"
-            "derrota" -> "Você perdeu, o adversário escolheu ${maoBot.name}"
-            else -> "Resultado indeterminado"
+            // SÓ DERROTAS
+            maoUsuario == JogadasMao.TESOURA && bot1 == JogadasMao.TESOURA && bot2 == JogadasMao.PEDRA-> exibirResultado("derrota", null)
+            maoUsuario == JogadasMao.TESOURA && bot1 == JogadasMao.PEDRA && bot2 == JogadasMao.TESOURA-> exibirResultado("derrota", null)
+            maoUsuario == JogadasMao.PAPEL && bot1 == JogadasMao.PAPEL && bot2 == JogadasMao.TESOURA-> exibirResultado("derrota", null)
+            maoUsuario == JogadasMao.PAPEL && bot1 == JogadasMao.TESOURA && bot2 == JogadasMao.PAPEL-> exibirResultado("derrota", null)
+            maoUsuario == JogadasMao.PEDRA && bot1 == JogadasMao.PEDRA && bot2 == JogadasMao.PAPEL-> exibirResultado("derrota", null)
+            maoUsuario == JogadasMao.PEDRA && bot1 == JogadasMao.PAPEL && bot2 == JogadasMao.PEDRA-> exibirResultado("derrota", null)
+            else -> exibirResultado("derrota", null)
         }
-        Toast.makeText(this, mensagem, Toast.LENGTH_SHORT).show()
+
+    }
+
+
+    private fun exibirResultado(resultado: String, maoBot: JogadasMao?) {
+        if(maoBot!=null){
+            val mensagem = when (resultado) {
+                "empate" -> "EMPATE, o adversário também escolheu ${maoBot.name}"
+                "vitória" -> "ViTÓRIA, o adversário escolheu ${maoBot.name}"
+                "derrota" -> "DERROTA, o adversário escolheu ${maoBot.name}"
+                else -> "Resultado indeterminado"
+            }
+            Toast.makeText(this, mensagem, Toast.LENGTH_LONG).show()}
+        else{
+            val mensagem = when (resultado) {
+                "empate" -> "EMPATE"
+                "vitória" -> "ViTÓRIA"
+                "derrota" -> "DERROTA"
+                else -> "Resultado indeterminado"
+            }
+            Toast.makeText(this, mensagem, Toast.LENGTH_LONG).show()}
+    }
+    private fun getEscolhaBot(choice: JogadasMao): Int {
+        return when (choice) {
+            JogadasMao.PEDRA -> R.drawable.pedra
+            JogadasMao.PAPEL-> R.drawable.papel
+            JogadasMao.TESOURA -> R.drawable.tesoura
+            else -> {
+                Toast.makeText(this@GameActivity, "", Toast.LENGTH_LONG).show()
+                R.drawable.tesoura
+            }
+        }
     }
 }
